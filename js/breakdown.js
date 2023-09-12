7d1eb94545497dcf502f85d8d2c1c16c957081ba
Renan Mougenot Breviglieri Silva <mougenot.renan@gmail.com>
2023-09-12 11:23:23 -0600

    Sept 12

--- js/breakdown.js
+++ js/breakdown.js
@@ -1,9 +1,9 @@
 
 let keepers = ["Gustavo", "Bagley"];
-let defenders = ["Gabe", "Aziz", "Belal", "Danny", "Cooper","Kolton","Christian", "Ethan Barnette"];
-let mids = ["Cache", "Alex Gil", "Julio", "Wilo", "Riley", "Corbin", "Easton", "Boston", "Asher", "Michael Fuentes"];
-let strikers = ["Pearce", "Humberto", "Hunter Spencer", "Ben Gladwell", "Ricky"];
-let practice = ["Lorenzo Venega", "Damian", "Will Naughton", "Davon", "Mauricio Olivera", "Jorge Benitez", "Clement Noble"];
+let defenders = ["Gabe", "Aziz", "Belal", "Danny", "Cooper","Kolton","Christian", "Ethan Barnette","Damian"];
+let mids = ["Cache", "Alex Gil", "Julio", "Wilo", "Riley", "Easton", "Boston", "Asher", "Michael Fuentes", "Abyiath"];
+let strikers = ["Pearce", "Humberto", "Hunter Spencer", "Ben Gladwell"];
+let practice = ["Lorenzo Venega", "Will Naughton", "Davon", "Mauricio Olivera", "Jorge Benitez", "Clement Noble", "Luis Guzman", "Thomas Farr", "Adam Ferre"];
 
 document.getElementById("st").innerHTML = strikers.length;
 document.getElementById("mid").innerHTML = mids.length;
@@ -14,8 +14,8 @@ document.getElementById("total").innerHTML = strikers.length + mids.length + def
 document.getElementById("totalp").innerHTML = strikers.length + mids.length + defenders.length + keepers.length + practice.length;
 
 
-let cams = ["Cache", "Julio", "Riley", "Corbin", "Easton", "Ricky", "Pearce", "Aziz",, "Michael Fuentes"];
-let cms = ["Cache", "Julio", "Alex Gil", "Wilo", "Riley", "Corbin", "Easton", "Boston", "Gabe", "Michael Fuentes"];
+let cams = ["Cache", "Julio", "Riley", "Corbin", "Easton", "Pearce", "Aziz", "Michael Fuentes"];
+let cms = ["Cache", "Julio", "Alex Gil", "Wilo", "Riley", "Easton", "Boston", "Gabe", "Michael Fuentes"];
 let cdms = ["Alex Gil", "Wilo", "Boston", "Cooper", "Kolton", "Gabe", "Julio"];
 let cbs = ["Gabe", "Cooper","Kolton", "Damian", "Ethan Barnette"];
 let obs = ["Gabe", "Aziz", "Belal", "Danny", "Asher", "Christian", "Ethan Barnette","Cooper", "Alex Gil"];
